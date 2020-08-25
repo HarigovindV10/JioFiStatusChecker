470bc19c80dd4421ffb083d8e880632a385dba15
HarigovindV10 <harigovindv.srt@gmail.com>
2020-08-26 00:22:56 +0530

    Initial Commit

---
+++ JioFiStatusChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JioFiStatusChecker
+//
+//  Created by HARIGOVIND VALSAKUMAR on 26/08/20.
+//  Copyright © 2020 HARIGOVIND VALSAKUMAR. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
